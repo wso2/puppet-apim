572b688569434db818f48145ed145dd2c91ff44b
Samitha Chathuranga <rmschathuranga@gmail.com>
2017-01-04 00:00:54 +0530

    Change jndi name of MB store db

--- manifests/params.pp
+++ manifests/params.pp
@@ -228,7 +228,7 @@ class wso2am::params {
         url                 => 'jdbc:h2:repository/database/WSO2MB_DB;DB_CLOSE_ON_EXIT=FALSE;LOCK_TIMEOUT=60000',
         username            => 'wso2carbon',
         password            => 'wso2carbon',
-        jndi_config         => 'WSO2MBStoreDB',
+        jndi_config         => 'WSO2_MB_STORE_DB',
         max_active          => '50',
         max_wait            => '60000',
         test_on_borrow      => true,
