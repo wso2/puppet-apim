d69be3f1323ce91ff71634c76309044e1853215f
irunika <uvoofLakmal075>
2018-09-30 13:35:58 +0530

    Add APIM analytics dashboard profile

---
+++ modules/apim_analytics_dashboard/manifests/init.pp
@@ -0,0 +1,118 @@
+# ----------------------------------------------------------------------------
+#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ----------------------------------------------------------------------------
+
+# Class: apim_analytics_dashboard
+# Init class of API Manager default profile
+class apim_analytics_dashboard inherits apim_analytics_dashboard::params {
+
+  if $::osfamily == 'redhat' {
+    $apim_analytics_package = 'wso2am-analytics-linux-installer-x64-2.6.0.rpm'
+    $installer_provider = 'rpm'
+    $install_path = '/usr/lib64/wso2/wso2am-analytics/2.6.0'
+  }
+  elsif $::osfamily == 'debian' {
+    $apim_analytics_package = 'wso2am-analytics-linux-installer-x64-2.6.0.deb'
+    $installer_provider = 'dpkg'
+    $install_path = '/usr/lib/wso2/wso2am-analytics/2.6.0'
+  }
+
+  # Create wso2 group
+  group { $user_group:
+    ensure => present,
+    gid    => $user_group_id,
+    system => true,
+  }
+
+  # Create wso2 user
+  user { $user:
+    ensure => present,
+    uid    => $user_id,
+    gid    => $user_group_id,
+    home   => "/home/${user}",
+    system => true,
+  }
+  # Ensure the installation directory is available
+  file { "/opt/${product}":
+    ensure => 'directory',
+    owner  => $user,
+    group  => $user_group,
+  }
+
+  # Copy the installer to the directory
+  file { "/opt/${product}/${apim_analytics_package}":
+    owner  => $user,
+    group  => $user_group,
+    mode   => '0644',
+    source => "puppet:///modules/${module_name}/${apim_analytics_package}",
+  }
+
+  # Install WSO2 API Manager
+  package { $product:
+    ensure   => installed,
+    provider => $installer_provider,
+    source   => "/opt/${product}/${apim_analytics_package}"
+  }
+
+  # Change the ownership of the installation directory to wso2 user & group
+  file { $install_path:
+    ensure  => directory,
+    owner   => $user,
+    group   => $user_group,
+    require => [ User[$user], Group[$user_group]],
+    recurse => true
+  }
+
+  # Copy configuration changes to the installed directory
+  $template_list.each | String $template | {
+    file { "${install_path}/${template}":
+      ensure  => file,
+      owner   => $user,
+      group   => $user_group,
+      mode    => '0644',
+      content => template("${module_name}/carbon-home/${template}.erb")
+    }
+  }
+
+  # Copy wso2server.sh to installed directory
+  file { "${install_path}/${start_script_template}":
+    ensure  => file,
+    owner   => $user,
+    group   => $user_group,
+    mode    => '0754',
+    content => template("${module_name}/carbon-home/${start_script_template}.erb")
+  }
+
+  # Copy the unit file required to deploy the server as a service
+  file { "/etc/systemd/system/${service_name}.service":
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0754',
+    content => template("${module_name}/${service_name}.service.erb"),
+  }
+
+  /*
+    Following script can be used to copy file to a given location.
+    This will copy some_file to install_path -> repository.
+    Note: Ensure that file is available in modules -> apim -> files
+  */
+  # file { "${install_path}/repository/some_file":
+  #   owner  => $user,
+  #   group  => $user_group,
+  #   mode   => '0644',
+  #   source => "puppet:///modules/${module_name}/some_file",
+  # }
+}
---
+++ modules/apim_analytics_dashboard/manifests/params.pp
@@ -0,0 +1,88 @@
+# ----------------------------------------------------------------------------
+#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ----------------------------------------------------------------------------
+
+# Claas apim_analytics_dashboard::params
+# This class includes all the necessary parameters.
+class apim_analytics_dashboard::params {
+  $user = 'wso2carbon'
+  $user_id = 802
+  $user_group = 'wso2'
+  $user_home = '/home/$user'
+  $user_group_id = 802
+  $product = 'wso2am-analytics'
+  $profile = 'dashboard'
+  $service_name = "${product}-${profile}"
+  $hostname = 'localhost'
+  $mgt_hostname = 'localhost'
+  $jdk_version = 'jdk1.8.0_192'
+
+  # Define the template
+  $start_script_template = "bin/${profile}.sh"
+  $template_list = [
+    'conf/dashboard/deployment.yaml'
+  ]
+
+  # -------------- Deploymeny.yaml Config -------------- #
+
+  # Carbon Configuration Parameters
+  $ports_offset = 3
+
+  # Configuration used for the databridge communication
+  $databridge_keystore = '${sys:carbon.home}/resources/security/wso2carbon.jks'
+  $databridge_keystore_password = 'wso2carbon'
+  $binary_data_receiver_hostname = '0.0.0.0'
+
+  # Configuration of the Data Agents - to publish events through
+  $thrift_agent_trust_store = '${sys:carbon.home}/resources/security/client-truststore.jks'
+  $thrift_agent_trust_store_password = 'wso2carbon'
+  $binary_agent_trust_store = '${sys:carbon.home}/resources/security/client-truststore.jks'
+  $binary_agent_trust_store_password = 'wso2carbon'
+
+  # Secure Vault Configuration
+  $securevault_key_store = '${sys:carbon.home}/resources/security/securevault.jks'
+  $securevault_private_key_alias = 'wso2carbon'
+  $securevault_secret_properties_file = '${sys:carbon.home}/conf/${sys:wso2.runtime}/secrets.properties'
+  $securevault_master_key_reader_file = '${sys:carbon.home}/conf/${sys:wso2.runtime}/master-keys.yaml'
+
+  # Data Sources Configuration
+  $business_rules_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/${sys:wso2.runtime}/database/BUSINESS_RULES_DB;DB_CLOSE_ON_EXIT=FALSE;LOCK_TIMEOUT=60000;MVCC=TRUE'
+  $business_rules_db_username = 'wso2carbon'
+  $business_rules_db_password = 'wso2carbon'
+  $business_rules_db_driver = 'org.h2.Driver'
+
+  $status_dashboard_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/${sys:wso2.runtime}/database/wso2_status_dashboard;DB_CLOSE_ON_EXIT=FALSE;LOCK_TIMEOUT=60000;MVCC=TRUE'
+  $status_dashboard_db_username = 'wso2carbon'
+  $status_dashboard_db_password = 'wso2carbon'
+  $status_dashboard_db_driver = 'org.h2.Driver'
+
+  $metrics_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/dashboard/database/metrics;AUTO_SERVER=TRUE'
+  $metrics_db_username = 'wso2carbon'
+  $metrics_db_password = 'wso2carbon'
+  $metrics_db_driver = 'org.h2.Driver'
+
+  # wso2.business.rules.manager config
+  $business_rules_manager_username = 'admin'
+  $business_rules_manager_password = 'admin'
+
+  # wso2.status.dashboard config
+  $status_dashboard_username = 'admin'
+  $status_dashboard_password = 'admin'
+
+  # transport.http configuration
+  $default_listener_keystore = '${carbon.home}/resources/security/wso2carbon.jks'
+  $default_listener_keystore_password = 'wso2carbon'
+  $default_listener_keystore_cert_pass = 'wso2carbon'
+}
