0f319279db2ffe8208afa7222487d61549e0e7ba
Savidude <mysticsavy@gmail.com>
2019-04-25 10:57:34 +0530

    Update APIM puppet resources for continuous update delivery

--- modules/apim_master/manifests/custom.pp
+++ modules/apim_master/manifests/custom.pp
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#  Copyright (c) 2019 WSO2, Inc. http://www.wso2.org
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -14,21 +14,8 @@
 #  limitations under the License.
 # ----------------------------------------------------------------------------
 
-# Class apim::startserver
-# Starts the server as a service in the final stage.
-class apim::startserver (
-  $service_name = $apim::params::service_name
-)
-  inherits apim::params {
-
-  exec { 'daemon-reload':
-    command => "systemctl daemon-reload",
-    path    => "/bin/",
-  }
-
-  service { $service_name:
-    enable => true,
-    ensure => running,
-    subscribe => File["binary"],
-  }
+# Class: apim::custom
+# This class is reserved to run custom user code before starting the server.
+class apim_master::custom {
+  # resources
 }
