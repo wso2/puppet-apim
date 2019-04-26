f5dcddbd5e4d99f9eb794b9ffdb6b6958c02e701
Savidude <mysticsavy@gmail.com>
2019-04-26 13:35:14 +0530

    Update tm puppet resources for continuous update delivery

--- modules/apim_tm_master/manifests/custom.pp
+++ modules/apim_tm_master/manifests/custom.pp
@@ -14,21 +14,8 @@
 #  limitations under the License.
 # ----------------------------------------------------------------------------
 
-# Class apim::startserver
-# Starts the server as a service in the final stage.
-class apim_tm::startserver (
-  $service_name = $apim_tm::params::service_name
-)
-  inherits apim_tm::params {
-
-  exec { 'daemon-reload':
-    command => "systemctl daemon-reload",
-    path    => "/bin/",
-  }
-
-  service { $service_name:
-    enable => true,
-    ensure => running,
-    subscribe => File["binary"],
-  }
+# Class: apim::custom
+# This class is reserved to run custom user code before starting the server.
+class apim_tm_master::custom {
+  # resources
 }
---
+++ modules/apim_tm_master/manifests/init.pp
@@ -0,0 +1,76 @@
+# ----------------------------------------------------------------------------
+#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ----------------------------------------------------------------------------
+
+# Class: apim_gateway
+# Init class of API Manager gateway profile
+class apim_tm_master inherits apim_tm_master::params {
+
+  # Create distribution path
+  file { [  "${products_dir}",
+    "${products_dir}/${product}",
+    "${distribution_path}"]:
+    ensure => 'directory',
+  }
+
+  # Copy binary to distribution path
+  file { "binary":
+    path   => "${distribution_path}/${product_binary}",
+    mode   => '0644',
+    source => "puppet:///modules/${module_name}/${product_binary}",
+  }
+
+  # Install the "unzip" package
+  package { 'unzip':
+    ensure => installed,
+  }
+
+  # Unzip the binary and create setup
+  exec { "unzip-binary":
+    command     => "unzip ${product_binary}",
+    path        => "/usr/bin/",
+    cwd         => $distribution_path,
+    onlyif      => "/usr/bin/test ! -d ${install_path}",
+    subscribe   => File["binary"],
+    refreshonly => true,
+    require     => Package['unzip'],
+  }
+
+  # Copy configuration changes to the installed directory
+  $template_list.each |String $template| {
+    file { "${install_path}/${template}":
+      ensure  => file,
+      mode    => '0644',
+      content => template("${module_name}/carbon-home/${template}.erb")
+    }
+  }
+
+  # Install the "zip" package
+  package { 'zip':
+    ensure => installed,
+  }
+
+  /*
+    Following script can be used to copy file to a given location.
+    This will copy some_file to install_path -> repository.
+    Note: Ensure that file is available in modules -> apim_tm_master -> files
+  */
+  # file { "${install_path}/repository/some_file":
+  #   owner  => $user,
+  #   group  => $user_group,
+  #   mode   => '0644',
+  #   source => "puppet:///modules/${module_name}/some_file",
+  # }
+}
