db942003d7468ca15d31a06603df79d1e53fb214
Savidude <mysticsavy@gmail.com>
2019-04-26 16:37:36 +0530

    Update analytics dashboard resources for continuous update delivery

--- modules/apim_analytics_dashboard/manifests/params.pp
+++ modules/apim_analytics_dashboard/manifests/params.pp
@@ -14,87 +14,34 @@
 #  limitations under the License.
 # ----------------------------------------------------------------------------
 
-# Claas apim_analytics_dashboard::params
+# Class apim_analytics_dashboard::params
 # This class includes all the necessary parameters.
 class apim_analytics_dashboard::params {
   $user = 'wso2carbon'
-  $user_id = 802
   $user_group = 'wso2'
-  $user_home = '/home/$user'
-  $user_group_id = 802
   $product = 'wso2am-analytics'
   $product_version = '2.6.0'
   $profile = 'dashboard'
   $service_name = "${product}-${profile}"
-  $hostname = 'localhost'
-  $mgt_hostname = 'localhost'
-  $jdk_version = 'jdk1.8.0_192'
+
+  # JDK Distributions
+  if $::osfamily == 'redhat' {
+    $lib_dir = "/usr/lib64/wso2"
+  }
+  elsif $::osfamily == 'debian' {
+    $lib_dir = "/usr/lib/wso2"
+  }
+  $jdk_name = 'amazon-corretto-8.202.08.2-linux-x64'
+  $java_home = "${lib_dir}/${jdk_name}"
 
   # Define the template
   $start_script_template = "bin/${profile}.sh"
-  $template_list = [
-    'conf/dashboard/deployment.yaml'
-  ]
 
-  # -------------- Deploymeny.yaml Config -------------- #
+  # Directories
+  $products_dir = "/usr/local/wso2"
 
-  # Carbon Configuration Parameters
-  $ports_offset = 3
-
-  # Configuration used for the databridge communication
-  $databridge_keystore = '${sys:carbon.home}/resources/security/wso2carbon.jks'
-  $databridge_keystore_password = 'wso2carbon'
-  $binary_data_receiver_hostname = '0.0.0.0'
-
-  # Configuration of the Data Agents - to publish events through
-  $thrift_agent_trust_store = '${sys:carbon.home}/resources/security/client-truststore.jks'
-  $thrift_agent_trust_store_password = 'wso2carbon'
-  $binary_agent_trust_store = '${sys:carbon.home}/resources/security/client-truststore.jks'
-  $binary_agent_trust_store_password = 'wso2carbon'
-
-  # Secure Vault Configuration
-  $securevault_key_store = '${sys:carbon.home}/resources/security/securevault.jks'
-  $securevault_private_key_alias = 'wso2carbon'
-  $securevault_secret_properties_file = '${sys:carbon.home}/conf/${sys:wso2.runtime}/secrets.properties'
-  $securevault_master_key_reader_file = '${sys:carbon.home}/conf/${sys:wso2.runtime}/master-keys.yaml'
-
-  # Data Sources Configuration
-  $business_rules_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/${sys:wso2.runtime}/database/BUSINESS_RULES_DB;DB_CLOSE_ON_EXIT=FALSE;LOCK_TIMEOUT=60000;MVCC=TRUE'
-  $business_rules_db_username = 'wso2carbon'
-  $business_rules_db_password = 'wso2carbon'
-  $business_rules_db_driver = 'org.h2.Driver'
-
-  $status_dashboard_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/${sys:wso2.runtime}/database/wso2_status_dashboard;DB_CLOSE_ON_EXIT=FALSE;LOCK_TIMEOUT=60000;MVCC=TRUE'
-  $status_dashboard_db_username = 'wso2carbon'
-  $status_dashboard_db_password = 'wso2carbon'
-  $status_dashboard_db_driver = 'org.h2.Driver'
-
-  $metrics_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/dashboard/database/metrics;AUTO_SERVER=TRUE'
-  $metrics_db_username = 'wso2carbon'
-  $metrics_db_password = 'wso2carbon'
-  $metrics_db_driver = 'org.h2.Driver'
-
-  $permission_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/${sys:wso2.runtime}/database/PERMISSION_DB;IFEXISTS=TRUE;DB_CLOSE_ON_EXIT=FALSE;LOCK_TIMEOUT=60000;MVCC=TRUE'
-  $permission_db_username = 'wso2carbon'
-  $permission_db_password = 'wso2carbon'
-  $permission_db_driver = 'org.h2.Driver'
-
-  $apim_analytics_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/worker/database/WSO2AM_STATS_DB;AUTO_SERVER=TRUE'
-  $apim_analytics_db_username = 'wso2carbon'
-  $apim_analytics_db_password = 'wso2carbon'
-  $apim_analytics_db_driver = 'org.h2.Driver'
-
-  # wso2.business.rules.manager config
-  $business_rules_manager_username = 'admin'
-  $business_rules_manager_password = 'admin'
-
-  # wso2.status.dashboard config
-  $status_dashboard_username = 'admin'
-  $status_dashboard_password = 'admin'
-
-  # transport.http configuration
-  $default_host = '0.0.0.0'
-  $default_listener_keystore = '${carbon.home}/resources/security/wso2carbon.jks'
-  $default_listener_keystore_password = 'wso2carbon'
-  $default_listener_keystore_cert_pass = 'wso2carbon'
+  # Product and installation information
+  $product_binary = "${product}-${product_version}.zip"
+  $distribution_path = "${products_dir}/${product}/${profile}/${product_version}"
+  $install_path = "${distribution_path}/${product}-${product_version}"
 }
