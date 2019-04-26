502a233407a74817ac3dc05b980d4250d9c04b76
Savidude <mysticsavy@gmail.com>
2019-04-26 12:33:22 +0530

    Update publisher puppet resources for continuous update delivery

--- modules/apim_publisher_master/manifests/startserver.pp
+++ modules/apim_publisher_master/manifests/startserver.pp
@@ -16,19 +16,7 @@
 
 # Class apim::startserver
 # Starts the server as a service in the final stage.
-class apim_publisher::startserver (
-  $service_name = $apim_publisher::params::service_name
-)
-  inherits apim_publisher::params {
+class apim_publisher_master::startserver inherits apim_publisher_master::params {
 
-  exec { 'daemon-reload':
-    command => "systemctl daemon-reload",
-    path    => "/bin/",
-  }
-
-  service { $service_name:
-    enable => true,
-    ensure => running,
-    subscribe => File["binary"],
-  }
+  # This class should be used to control Identity Server if required.
 }
