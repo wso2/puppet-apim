439b28113c53803399ff3c67086e7876313acf52
Savidude <mysticsavy@gmail.com>
2019-04-26 17:10:16 +0530

    Update analytics worker resources for continuous update delivery

---
+++ modules/apim_analytics_worker_master/manifests/params.pp
@@ -0,0 +1,95 @@
+# ----------------------------------------------------------------------------
+#  Copyright (c) 2019 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ----------------------------------------------------------------------------
+
+# Class apim_analytics_dashboard::params
+# This class includes all the necessary parameters.
+class apim_analytics_worker_master::params {
+  $user = 'wso2carbon'
+  $user_group = 'wso2'
+  $product = 'wso2am-analytics'
+  $product_version = '2.6.0'
+  $profile = 'worker'
+
+  # Define the template
+  $template_list = [
+    'conf/dashboard/deployment.yaml'
+  ]
+
+  # -------------- Deployment.yaml Config -------------- #
+
+  # Carbon Configuration Parameters
+  $ports_offset = 1
+
+  # transport.http config
+  $default_listener_host = '0.0.0.0'
+  $msf4j_host = '0.0.0.0'
+  $msf4j_listener_keystore = '${carbon.home}/resources/security/wso2carbon.jks'
+  $msf4j_listener_keystore_password = 'wso2carbon'
+  $msf4j_listener_keystore_cert_pass = 'wso2carbon'
+
+  # siddhi.stores.query.api config
+  $siddhi_default_listener_host = '0.0.0.0'
+  $siddhi_msf4j_host = '0.0.0.0'
+  $siddhi_msf4j_listener_keystore = '${carbon.home}/resources/security/wso2carbon.jks'
+  $siddhi_msf4j_listener_keystore_password = 'wso2carbon'
+  $siddhi_msf4j_listener_keystore_cert_pass = 'wso2carbon'
+
+  # Configuration used for the databridge communication
+  $databridge_keystore = '${sys:carbon.home}/resources/security/wso2carbon.jks'
+  $databridge_keystore_password = 'wso2carbon'
+  $binary_data_receiver_hostname = '0.0.0.0'
+
+  # Configuration of the Data Agents - to publish events through
+  $thrift_agent_trust_store = '${sys:carbon.home}/resources/security/client-truststore.jks'
+  $thrift_agent_trust_store_password = 'wso2carbon'
+  $binary_agent_trust_store = '${sys:carbon.home}/resources/security/client-truststore.jks'
+  $binary_agent_trust_store_password = 'wso2carbon'
+
+  # Secure Vault Configuration
+  $securevault_key_store = '${sys:carbon.home}/resources/security/securevault.jks'
+  $securevault_private_key_alias = 'wso2carbon'
+  $securevault_secret_properties_file = '${sys:carbon.home}/conf/${sys:wso2.runtime}/secrets.properties'
+  $securevault_master_key_reader_file = '${sys:carbon.home}/conf/${sys:wso2.runtime}/master-keys.yaml'
+
+  # Data Sources Configuration
+  $metrics_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/dashboard/database/metrics;AUTO_SERVER=TRUE'
+  $metrics_db_username = 'wso2carbon'
+  $metrics_db_password = 'wso2carbon'
+  $metrics_db_driver = 'org.h2.Driver'
+
+  $permission_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/${sys:wso2.runtime}/database/PERMISSION_DB;IFEXISTS=TRUE;DB_CLOSE_ON_EXIT=FALSE;LOCK_TIMEOUT=60000;MVCC=TRUE'
+  $permission_db_username = 'wso2carbon'
+  $permission_db_password = 'wso2carbon'
+  $permission_db_driver = 'org.h2.Driver'
+
+  $message_tracing_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/dashboard/database/MESSAGE_TRACING_DB;AUTO_SERVER=TRUE'
+  $message_tracing_db_username = 'wso2carbon'
+  $message_tracing_db_password = 'wso2carbon'
+  $message_tracing_db_driver = 'org.h2.Driver'
+
+  $apim_analytics_db_url = 'jdbc:h2:${sys:carbon.home}/wso2/worker/database/WSO2AM_STATS_DB;AUTO_SERVER=TRUE'
+  $apim_analytics_db_username = 'wso2carbon'
+  $apim_analytics_db_password = 'wso2carbon'
+  $apim_analytics_db_driver = 'org.h2.Driver'
+
+  # Directories
+  $products_dir = "/usr/local/wso2"
+
+  # Product and installation paths
+  $product_binary = "${product}-${product_version}.zip"
+  $distribution_path = "${products_dir}/${product}/${profile}/${product_version}"
+  $install_path = "${distribution_path}/${product}-${product_version}"
+}
