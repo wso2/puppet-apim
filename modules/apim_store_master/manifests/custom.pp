7f93decd28f61f816be6778d0935776edafcaad6
Savidude <mysticsavy@gmail.com>
2019-04-26 12:57:59 +0530

    Update store puppet resources for continuous update delivery

--- modules/apim_store_master/manifests/custom.pp
+++ modules/apim_store_master/manifests/custom.pp
@@ -14,21 +14,8 @@
 #  limitations under the License.
 # ----------------------------------------------------------------------------
 
-# Class apim::startserver
-# Starts the server as a service in the final stage.
-class apim_store::startserver (
-  $service_name = $apim_store::params::service_name
-)
-  inherits apim_store::params {
-
-  exec { 'daemon-reload':
-    command => "systemctl daemon-reload",
-    path    => "/bin/",
-  }
-
-  service { $service_name:
-    enable => true,
-    ensure => running,
-    subscribe => File["binary"],
-  }
+# Class: apim::custom
+# This class is reserved to run custom user code before starting the server.
+class apim_store_master::custom {
+  # resources
 }
