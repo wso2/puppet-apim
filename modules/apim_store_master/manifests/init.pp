3b96b09e7093fd268058209d3e8019aa78604aa3
Savidude <mysticsavy@gmail.com>
2019-04-30 16:01:07 +0530

    Move product distributions outside modules

--- modules/apim_store_master/manifests/init.pp
+++ modules/apim_store_master/manifests/init.pp
@@ -29,7 +29,7 @@ class apim_store_master inherits apim_store_master::params {
   file { "binary":
     path   => "${distribution_path}/${product_binary}",
     mode   => '0644',
-    source => "puppet:///modules/${module_name}/${product_binary}",
+    source => "puppet:///modules/distributions/${product_binary}",
   }
 
   # Install the "unzip" package
