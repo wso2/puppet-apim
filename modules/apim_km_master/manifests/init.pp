d60f048d4077c1f341bb030d125454b975ec416b
Savidude <mysticsavy@gmail.com>
2019-05-02 15:44:23 +0530

    Kill analytics servers with pid from runtime file

--- modules/apim_km_master/manifests/init.pp
+++ modules/apim_km_master/manifests/init.pp
@@ -21,6 +21,7 @@ class apim_km_master inherits apim_km_master::params {
   # Create distribution path
   file { [  "${products_dir}",
     "${products_dir}/${product}",
+    "${products_dir}/${product}/${profile}",
     "${distribution_path}"]:
     ensure => 'directory',
   }
