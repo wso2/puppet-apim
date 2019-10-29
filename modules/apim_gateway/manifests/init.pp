db6dffb1ab5df916a4289598e5c2ea332adb6970
Savidude <mysticsavy@gmail.com>
2019-10-29 17:10:53 +0530

    Update configurations for APIM Gateway 3.0.0

--- modules/apim_gateway/manifests/init.pp
+++ modules/apim_gateway/manifests/init.pp
@@ -20,6 +20,11 @@ class apim_gateway inherits apim_gateway::params {
 
   include apim_common
 
+  # Optimize for profile
+  exec { "optimize-profile":
+    command => "/bin/sh ${carbon_home}/bin/profileSetup.sh -Dprofile=gateway-worker",
+  }
+
   # Copy configuration changes to the installed directory
   $template_list.each |String $template| {
     file { "${carbon_home}/${template}":
